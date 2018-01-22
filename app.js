1ede0c8983cbd499c6f7f91b4d2588c8aa4044c1
Anton1501535 <antonjmakela@gmail.com>
2018-01-22 18:35:43 +0200

    assign22

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var about = require('./routes/about');
+var anton = require('./routes/anton');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/about', about);
+app.use('/anton', anton);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
